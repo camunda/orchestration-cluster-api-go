0c5068679280482025dbd333788b3190a5ef6910
Emily Oram <emily.oram@camunda.com>
2026-07-17 10:17:15 +1200

    chore(gen): relax DeploymentMetadataResult required members

--- client/model_deployment_metadata_result.go
+++ client/model_deployment_metadata_result.go
@@ -209,13 +209,7 @@ func (o *DeploymentMetadataResult) UnmarshalJSON(data []byte) (err error) {
 	// This validates that all required properties are included in the JSON object
 	// by unmarshalling the object into a generic map with string keys and checking
 	// that every required field exists as a key in the generic map.
-	requiredProperties := []string{
-		"processDefinition",
-		"decisionDefinition",
-		"decisionRequirements",
-		"form",
-		"resource",
-	}
+	requiredProperties := []string{}
 
 	allProperties := make(map[string]interface{})
 
